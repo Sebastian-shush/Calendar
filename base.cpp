65370da4246db21bd7b802b1b25ca58e0bbe0ddb
Sebastian-shush <70400042+Sebastian-shush@users.noreply.github.com>
2020-09-01 10:06:00 -0700

    Create base.cpp

---
+++ base.cpp
@@ -0,0 +1,29 @@
+int main() {
+  string months[12] = {"January", "February", "March", "April", "May", "June", "July",
+"August", "September", "October", "November", "December"};
+  months[0] = "Does not exist";
+  cout << months[0];
+  return 6;
+}
+
+int monthJanuary = 1;
+int monthFebruary = 2;
+int monthMarch = 3;
+int monthApril = 4;
+int monthMay = 5;
+int monthJune = 6;
+int monthJuly = 7;
+int monthAugust = 8;
+int monthSeptember = 9;
+int monthOctober = 10;
+int monthNovember = 11;
+int monthDecember = 12;
+/*int months[12] = ["January", "February", "March", "April", "May", "June", "July",
+"August", "September", "October", "November", "December"]
+print months[6]
+
+int calenderDays <= 31
+int calenderWeek <= 52
+int calendarMonths <= 12*/
+
+
